789d8434dcaa94c1b48da6fdf8c8b354748d85e8
danay415 <jayla.danay@yahoo.com>
2023-09-07 23:18:27 -0400

    created day 1 class and scanner

---
+++ AoC22Day1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AoC22Day1 {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+      Scanner scanner = new Scanner(System.in);
+
+      scanner.close();
+   }
+}
