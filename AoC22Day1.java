01dad86d7c88bc5093a53a2f2319047aee96f4a6
danay415 <jayla.danay@yahoo.com>
2023-09-08 14:45:07 -0400

    cleaned up Day 1 :)

--- AoC22Day1.java
+++ AoC22Day1.java
@@ -3,16 +3,9 @@ import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 
-public class AoC22Day1
-{
-
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
+public class AoC22Day1 {
    public static void main(String []args) throws Exception
    {
-
       // intake the input of elf calories and assign the text file to the scanner
       File file = new File("Day1Input.txt");
       Scanner scanner = new Scanner(file);
@@ -20,24 +13,24 @@ public class AoC22Day1
       int calorieCount = 0;
       int maxCal = 0;
       ArrayList<Integer> calories = new ArrayList<>();
-      while (scanner.hasNextLine())
-      {
+
+      while (scanner.hasNextLine()) {
          String cal;
-         if ((cal = scanner.nextLine()) != "")
-         {
+         if ((cal = scanner.nextLine()) != "") {
             calorieCount += Integer.parseInt(cal);
-         } else
-         {
-            if (calorieCount > maxCal)
-            {
-               maxCal = calorieCount;
-            }
-            calories.add(calorieCount);
-            calorieCount = 0;
+            continue; // continue skips the rest of the current loop and goes to the next iteration
          }
+
+         if (calorieCount > maxCal) {
+            maxCal = calorieCount;
+         }
+         calories.add(calorieCount);
+         calorieCount = 0;
       }
+   
       Collections.sort(calories, Collections.reverseOrder());
       int topThree = calories.get(0) + calories.get(1) + calories.get(2);
+
       scanner.close();
       System.out.println(topThree);
    }
---
+++ AoC22Day2.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class AoC22Day2 {
+    public static void main(String []args) throws Exception
+    {
+        File file = new File("Day2Input.txt");
+        Scanner scanner = new Scanner(file);
+
+        
+    }
+}
