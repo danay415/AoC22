4e19a40b856aa7cd9bf2fc7a74d537d26fdcc8c9
danay415 <jayla.danay@yahoo.com>
2023-09-08 00:33:34 -0400

    first half complete, elf with most cals

--- AoC22Day1.java
+++ AoC22Day1.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
 
-public class AoC22Day1 {
+public class AoC22Day1
+{
 
    /* This is my first java program.
     * This will print 'Hello World' as the output
     */
 
-   public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
-      Scanner scanner = new Scanner(System.in);
+   public static void main(String []args) throws Exception
+   {
 
+      // intake the input of elf calories and assign the text file to the scanner
+      File file = new File("Day1Input.txt");
+      Scanner scanner = new Scanner(file);
+
+      ArrayList<Integer> calories = new ArrayList<>();
+      int calorieCount = 0;
+      int maxCal = 0;
+      while (scanner.hasNextLine())
+      {
+         String cal;
+         if ((cal = scanner.nextLine()) != "")
+         {
+            calorieCount += Integer.parseInt(cal);
+         } else
+         {
+            if (calorieCount > maxCal)
+            {
+               maxCal = calorieCount;
+            }
+            calorieCount = 0;
+         }
+      }
       scanner.close();
+      System.out.println(maxCal);
    }
 }
