569f19859af0731ff9b68c12330d5ec860d24986
danay415 <jayla.danay@yahoo.com>
2023-09-12 14:46:40 -0400

    completed day 3 part 2!

--- AoC22Day3.java
+++ AoC22Day3.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class AoC22Day3 {
@@ -17,28 +15,54 @@ public class AoC22Day3 {
         items.add(Character.toString(c));
       }
 
-      String item = "";
-      int weight = 0;
-      while (scanner.hasNextLine()) {
-        String rucksack = scanner.nextLine();
-        String compartment1 = rucksack.substring(0, rucksack.length() / 2);
-        String compartment2 = rucksack.substring(rucksack.length() / 2, rucksack.length());
-        boolean found = false;
+      // PART 1
+      // String item = "";
+      // int weight = 0;
+      // boolean foundMatch = false;
 
-        for (int i = 0; i < rucksack.length() / 2; i++) {
-            for (int j = 0; j < rucksack.length() / 2; j++) {
-                if (compartment1.charAt(i) == compartment2.charAt(j) && !found) {
-                    item += compartment1.charAt(i);
-                    System.out.println(item);
-                    weight += items.indexOf(item) + 1;
-                    item = "";
-                    found = true;
-                }
+      // while (scanner.hasNextLine()) {
+      //   String rucksack = scanner.nextLine();
+      //   String compartment1 = rucksack.substring(0, rucksack.length() / 2);
+      //   String compartment2 = rucksack.substring(rucksack.length() / 2, rucksack.length());
+      //   foundMatch = false;
+
+      //   for (int i = 0; i < compartment1.length(); i++) {
+      //       for (int j = 0; j < compartment2.length(); j++) {
+      //           if (compartment1.charAt(i) == compartment2.charAt(j) && !foundMatch) {
+      //               item += compartment1.charAt(i);
+      //               weight += items.indexOf(item) + 1;
+      //               item = "";
+      //               foundMatch = true;
+      //           }
+      //       }
+      //   }
+      // }
+
+      int weight = 0;
+      ArrayList<String> possibleBadges = new ArrayList<>();
+
+      while (scanner.hasNextLine()) {
+        String elf1 = scanner.nextLine();
+        String elf2 = scanner.nextLine();
+        String elf3 = scanner.nextLine();
+
+        for (int i = 0; i < elf1.length(); i++) {
+          for (int j = 0; j < elf2.length(); j++) {
+            if (elf1.charAt(i) == elf2.charAt(j) && !possibleBadges.contains("" + elf1.charAt(i))) {
+              possibleBadges.add("" + elf1.charAt(i));
             }
+          }
         }
+        for (String item : possibleBadges) {
+          if (elf3.contains(item)) {
+            weight += items.indexOf(item) + 1;
+          }
+        }
+        possibleBadges.clear();
       }
 
     scanner.close();
     System.out.println(weight);
+
    }
 }
