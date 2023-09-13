e91367162f9ed984d7d78ffe05377a8e9a2d3e08
danay415 <jayla.danay@yahoo.com>
2023-09-13 17:06:54 -0400

    set up day 5

--- AoC22Day5.java
+++ AoC22Day5.java
@@ -1,5 +1,17 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AoC22Day5 {
     public static void main(String []args) throws Exception {
-    
+        File file = new File("Test.txt");
+        Scanner scanner = new Scanner(file);
+        ArrayList<String> test = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            test.add(scanner.nextLine());
+        }
+
+        scanner.close();
     }
 }
