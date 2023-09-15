870e003849eb1409c596464f49f35d505f049843
danay415 <jayla.danay@yahoo.com>
2023-09-14 22:05:57 -0400

    day 5 finished!

--- AoC22Day5.java
+++ AoC22Day5.java
@@ -4,27 +4,29 @@ import java.util.Scanner;
 
 public class AoC22Day5 {
     public static void main(String []args) throws Exception {
-        File file = new File("Test.txt");
+        File file = new File("Day5Input.txt");
         Scanner scanner = new Scanner(file);
         ArrayList<String> crates = new ArrayList<>();
 
         // scan up to the crate stacks
-        String instructions;
+        String cargo;
         while (scanner.hasNextLine()) {
-            instructions = scanner.nextLine();
-            if (instructions.equals("")) {
+            cargo = scanner.nextLine();
+            if (cargo.equals("")) {
                 break;
             }
-            crates.add(instructions);
+            crates.add(cargo);
         }
 
-        // initialize the stacks starting with the stack number
-        int numStacks = crates.get(0).length() / 3;
+        // convert the scanner info to rows
+        int numStacks = Integer.parseInt(crates.get(crates.size() - 1)
+        .substring(crates.get(crates.size() - 1).length() - 2, crates.get(crates.size() - 1).length() - 1));
         ArrayList<ArrayList<String>> stacks = new ArrayList<>();
+        ArrayList<ArrayList<String>> rows = new ArrayList<>();
+
         for (int i = 0; i < numStacks; i++) {
             ArrayList<String> currStack = new ArrayList<>();
-            currStack.add(Integer.toString(i + 1));
-            stacks.add(currStack);
+            rows.add(currStack);
         }
 
         // add the crates to the appropriate stacks
@@ -35,17 +37,72 @@ public class AoC22Day5 {
                     case " ":
                         whitespaceCount += 1;
                         if (whitespaceCount == 4) {
-                            stacks.get(i).add(" ");
+                            rows.get(i).add(" ");
                             whitespaceCount = 0;
                         }
                         break;
                     case "[":
-                        stacks.get(i).add(crates.get(i).substring(j, j + 3));
+                        whitespaceCount = 0;
+                        rows.get(i).add(crates.get(i).substring(j + 1, j + 2));
                         break;
                 }
             }
         }
+
+        // initialize the stacks starting with the stack number
+        for (int i = 0; i < numStacks; i++) {
+            ArrayList<String> currStack = new ArrayList<>();
+            stacks.add(currStack);
+        }
+
+        // convert rows to columns
+        for (int i = 0; i < rows.size(); i++) {
+            for (int j = 0; j < rows.get(i).size(); j++) {
+                if (!rows.get(i).get(j).equals(" ")) {
+                    stacks.get(j)
+                    .add(rows.get(i).get(j));
+                }
+            }
+        }
+        // get the instructions in an array
+        ArrayList<String> fullInstructions = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            fullInstructions.add(scanner.nextLine());
+        }
         scanner.close();
-        System.out.println(stacks);
+
+        // parse instructions: [amount, fromStack, toStack]
+        ArrayList<ArrayList<Integer>> instructions = new ArrayList<>();
+        for (String string : fullInstructions) {
+            String[] currSet = string.split(" ");
+            String amount = currSet[1];
+            String fromStack = currSet[3];
+            String toStack = currSet[5];
+            ArrayList<Integer> set = new ArrayList<>();
+            set.add(Integer.parseInt(amount));
+            set.add(Integer.parseInt(fromStack));
+            set.add(Integer.parseInt(toStack));
+            instructions.add(set);
+        }
+
+        // execute instructions
+        for (ArrayList<Integer> set : instructions) {
+            for (int i = set.get(0); i > 0; i--) {
+                ArrayList<String> takeFrom = stacks.get(set.get(1) - 1);
+                ArrayList<String> addTo = stacks.get(set.get(2) - 1);
+                if (takeFrom.size() > 0) {
+                    addTo.add(0, takeFrom.get(i - 1));
+                    takeFrom.remove(i - 1);
+                }
+            }
+        }
+
+        // get the top crate from each stack
+        String topCrates = "";
+        for (ArrayList<String> crate : stacks) {
+            topCrates += crate.get(0);
+        }
+
+        System.out.println(topCrates);
     }
 }
